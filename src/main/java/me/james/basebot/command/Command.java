86d5aa43bd668cc8d723fef69d7111d0f32716b7
electricman226 <electricman226@gmail.com>
2018-04-29 21:04:27 -0400

    Renamed Command#registerCommand, and added QoL changes.

--- src/main/java/me/james/basebot/command/Command.java
+++ src/main/java/me/james/basebot/command/Command.java
@@ -8,10 +8,28 @@ public abstract class Command
 {
     private static HashMap< String, Command > cmds = new HashMap<>();
 
+    @Deprecated
     public static void registerClass( String name, Command cmd )
+    {
+        registerCommand( name, cmd );
+    }
+
+    public static void registerCommand( String name, Command cmd )
     {
         cmds.putIfAbsent( name, cmd );
         Logger.getLogger( "BaseBot" ).info( "Registered command '" + name + "'." );
+    }
+
+    public static void removeCommand( String name )
+    {
+        Logger.getLogger( "BaseBot" ).info( "Removing command '" + name + "'." );
+        cmds.remove( name );
+    }
+
+    public static void removeCommand( Command cmd )
+    {
+        Logger.getLogger( "BaseBot" ).info( "Removing command class '" + cmd.getClass().getSimpleName() + "'." );
+        cmds.values().remove( cmd );
     }
 
     public static Command getCommand( String name )
